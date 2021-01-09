1315037c5e1de9528f4992e978cf6227c296dce5
Víctor Martínez <victorcoder2@gmail.com>
2021-01-09 01:29:37 +0100

    queries & git.md removed

--- server_rust/queries/all_users_with_last_location_timestamp.sql
+++
@@ -1,8 +0,0 @@
-SELECT users.username, users.email, latest_timestamp_utc FROM users LEFT JOIN (
-    SELECT MAX(device_telemetry.creation_timestamp) as latest_timestamp_utc, username
-    FROM device_telemetry
-    WHERE device_telemetry.creation_timestamp > current_timestamp - interval '{{lookback_days}} day'
-    GROUP BY device_telemetry.username
-) AS latest_updates ON latest_updates.username = users.username
-    [[WHERE users.email = {{email}} || users.phone_number = {{email}}]]
-ORDER BY latest_timestamp_utc asc
--- server_rust/queries/getlastlocation.sql
+++
@@ -1,4 +0,0 @@
-select username, recipient_username, creation_timestamp
-from device_telemetry
-where username = 'chelito'
-order by creation_timestamp desc;
