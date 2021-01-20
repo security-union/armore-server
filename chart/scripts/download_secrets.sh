2468d261079859b0bd77da72ff05a4dc934cc125
Griffin Obeid <griffobeid@gmail.com>
2021-01-20 18:33:43 -0500

    Adding helm

---
+++ chart/scripts/download_secrets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "No deployment name supplied."
+    exit 1;
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+deployment_name=$1
+
+berglas access ${BUCKET_ID}/${deployment_name} | base64 -d > $DIR/../secrets/${deployment_name}.yaml
