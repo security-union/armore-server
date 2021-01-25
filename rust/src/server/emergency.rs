8b0299246cfb090790e1e386f20a0eda72401b5f
Víctor Martínez <victorcoder2@gmail.com>
2021-01-25 19:54:07 +0100

    [ARM-135] Report emergency for a friend in progress

--- rust/src/server/emergency.rs
+++ rust/src/server/emergency.rs
@@ -1,4 +1,5 @@
 use super::middleware::{catchers::catchers, cors::options};
+use super::validators::friends::assert_not_friends;
 use crate::{
     controllers::emergency::{get_historical_location, update_user_state},
     db::{get_connection, get_pool},
@@ -6,7 +7,7 @@ use crate::{
     model::{
         auth::AuthInfo,
         emergency::{UpdateState, UserState},
-        responses::APIJsonResponse,
+        responses::{APIJsonResponse, APIResponse},
         telemetry::{DateTimeRange, Location},
         APIResult, Message, Storage,
     },
@@ -26,10 +27,41 @@ fn update_state(
     let new_state = update_state.new_state;
     get_connection(storage)
         .and_then(|conn| unlock_channel(rabbit).map(|channel| (conn, channel)))
-        .map_err(|err| APIJsonResponse::api_error_with_internal_error(err, &auth_info.language))
         .and_then(|(mut conn, channel)| {
-            update_user_state(&mut conn, &channel, &auth_info, &new_state)
+            update_user_state(&mut conn, &channel, &auth_info.username, &new_state)
         })
+        .and_then(|_| {
+            Ok(Json(APIResponse {
+                success: true,
+                result: Some(Message { message: new_state }),
+            }))
+        })
+        .map_err(|err| APIJsonResponse::api_error_with_internal_error(err, &auth_info.language))
+}
+
+#[post("/<username>/emergency")]
+fn report_friend_emergency(
+    auth_info: AuthInfo,
+    username: String,
+    storage: State<Storage>,
+    rabbit: State<Arc<Mutex<RabbitConnection>>>,
+) -> APIResult<Message<UserState>> {
+    get_connection(storage)
+        .and_then(|conn| unlock_channel(rabbit).map(|channel| (conn, channel)))
+        .and_then(|(mut conn, channel)| {
+            assert_not_friends(&mut conn, &auth_info.username, &username).and_then(|_| {
+                update_user_state(&mut conn, &channel, &username, &UserState::Emergency)
+            })
+        })
+        .and_then(|_| {
+            Ok(Json(APIResponse {
+                success: true,
+                result: Some(Message {
+                    message: UserState::Emergency,
+                }),
+            }))
+        })
+        .map_err(|err| APIJsonResponse::api_error_with_internal_error(err, &auth_info.language))
 }
 
 #[get("/<username>/telemetry?<start_time>&<end_time>")]
@@ -43,8 +75,14 @@ fn get_user_historical_location(
     let date_range = DateTimeRange::from_str(&start_time, &end_time)
         .map_err(|err| APIJsonResponse::api_error(err, None))?;
     get_connection(storage)
-        .map_err(|err| APIJsonResponse::api_error_with_internal_error(err, &auth_info.language))
         .and_then(|mut conn| get_historical_location(&mut conn, &auth_info, &username, &date_range))
+        .and_then(|historical| {
+            Ok(Json(APIResponse {
+                success: true,
+                result: Some(historical),
+            }))
+        })
+        .map_err(|err| APIJsonResponse::api_error_with_internal_error(err, &auth_info.language))
 }
 
 pub fn rocket() -> Rocket {
@@ -52,7 +90,10 @@ pub fn rocket() -> Rocket {
     let rabbit_conn = RabbitConnection::insecure_open(&get_rabbitmq_uri())
         .expect("Error getting rabbitMq Connection");
     rocket::ignite()
-        .mount("/v1/emergency", routes![update_state, get_user_historical_location])
+        .mount(
+            "/v1/emergency",
+            routes![update_state, get_user_historical_location],
+        )
         .register(catchers())
         .attach(options())
         .manage(Storage {
