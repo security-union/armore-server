e081b91dc57b1b00792330e7aa193eeeb0ea81d2
Griffin Obeid <griffobeid@gmail.com>
2021-01-30 10:19:50 -0500

    ARM-171 Terraform config, documentation, chart improvements (#17)
    * Adding redis
    
    * Adding helm
    
    * Deployed to staging
    
    * Adding resource constraints to services
    
    * Creating terraform config. Moving helm chart to deploy folder
    
    * Adding read gcr service account to terraform
    
    * Adding terraform docs and removing bad service account
    
    * save
    
    * Working terraform config
    
    * Updating network
    
    * Documenting the cluster creation process
    
    * Fixing order of dns route
    
    * Documented and deployed tls
    
    * Missing doc
    
    * Pin rust version on ci

---
+++ deploy/terraform/dev/main.tf
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2021 Security Union LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "gke" {
+  source                            = "terraform-google-modules/kubernetes-engine/google"
+  project_id                        = var.project_id
+  name                              = "${var.name}-cluster"
+  region                            = var.region
+  regional                          = true
+  network                           = module.gke-network.network_name
+  subnetwork                        = module.gke-network.subnets_names[0]
+  ip_range_pods                     = module.gke-network.subnets_secondary_ranges[0].*.range_name[0]
+  ip_range_services                 = module.gke-network.subnets_secondary_ranges[0].*.range_name[1]
+  grant_registry_access             = true
+  horizontal_pod_autoscaling        = true
+  service_account                   = "create"
+  remove_default_node_pool          = true
+  disable_legacy_metadata_endpoints = true
+
+  node_pools = [
+    {
+      name               = "${var.name}-node-pool"
+      machine_type       = "e2-small"
+      min_count          = 1
+      max_count          = 3 
+      disk_size_gb       = 30 
+      disk_type          = "pd-ssd"
+      image_type         = "COS"
+      auto_repair        = true
+      auto_upgrade       = false
+      preemptible        = false
+      initial_node_count = 1
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/cloud-platform",      
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+  }
+
+  node_pools_metadata = {
+    all = {}
+  }
+
+  node_pools_tags = {
+    all = []
+  }
+}
