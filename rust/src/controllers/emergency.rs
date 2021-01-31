e1c4b131c095352d3ee037410109ada27e2158e7
Víctor Martínez <victorcoder2@gmail.com>
2021-01-31 16:13:55 +0100

    [ARM-199] tests: added one test to test users with no email

--- rust/src/controllers/emergency.rs
+++ rust/src/controllers/emergency.rs
@@ -135,7 +135,7 @@ fn build_emergency_notifications(
         .map_or((vec![], None), |rec_details| {
             let notifications =
                 build_emergency_notification(conn, &sender_details, &rec_details, &state);
-            let email = sender_details.email.clone().map(|_| build_emergency_email(&sender_details, &rec_details, &state));
+            let email = rec_details.email.clone().and_then(|_| Some(build_emergency_email(&sender_details, &rec_details, &state)));
             (notifications, email)
         })
 }
