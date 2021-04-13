5b15feb48c237a4a55764b606668279f13048a44
Dario Alessandro Lencina Talarico <darioalessandrolencina@gmail.com>
2021-04-13 07:43:57 -0400

    save

--- nodejs/src/notification_server/index.spec.ts
+++ nodejs/src/notification_server/index.spec.ts
@@ -7,18 +7,41 @@ process.env.LABS_MOBILE_PASSWORD = "blah"
 import { SMSSender } from "./sms-sender";
 
 
-describe("Notifications Server", () => {
+describe.only("Notifications Server", () => {
     describe("SMSSender", () => {
         let sender: SMSSender
         beforeAll(() => {
             sender = new SMSSender()
         });
         test("Sends MEX sms to labsMobile", () => {
-            const phoneNumber = "+524771234333"
-
+            const phoneNumber = "+524771234333";
+            const smsRequest = {
+                body: "bla",
+                to: phoneNumber
+            };
+            const [twilio, labsMobile] = sender.mapPhoneNumberToProvider([smsRequest]);
+            expect(twilio).toHaveLength(0);
+            expect(labsMobile).toHaveLength(1);
         });
         test("Sends USA sms to twilio", () => {
-
+            const phoneNumber = "+12455436345";
+            const smsRequest = {
+                body: "bla",
+                to: phoneNumber
+            };
+            const [twilio, labsMobile] = sender.mapPhoneNumberToProvider([smsRequest]);
+            expect(twilio).toHaveLength(1);
+            expect(labsMobile).toHaveLength(0)
+        });
+        test("Sends COL sms to LabsMobile", () => {
+            const phoneNumber = "+573044469588";
+            const smsRequest = {
+                body: "bla",
+                to: phoneNumber
+            };
+            const [twilio, labsMobile] = sender.mapPhoneNumberToProvider([smsRequest]);
+            expect(twilio).toHaveLength(0);
+            expect(labsMobile).toHaveLength(1)
         });
     })
 });
